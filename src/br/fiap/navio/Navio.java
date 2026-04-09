c2131e532fba0a52c37f0ba5a479f59e032ddd5f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 09:46:50 -0300

    exercicio parcialmente finalizado

---
+++ src/br/fiap/navio/Navio.java
@@ -0,0 +1,44 @@
+package br.fiap.navio;
+
+import br.fiap.carga.Carga;
+import br.fiap.cliente.Cliente;
+
+//CLASSE VIAGEM
+public class Navio {
+    private double capacidade;
+    private int index;
+    private Carga[] carga;
+
+    public Navio() {
+        this.capacidade = 10000;
+        this.carga = new Carga[20];
+
+    }
+
+    public boolean reservar(Carga carga) {
+        if (index < this.carga.length) {
+            if (permitidoReservar(carga.getPeso())) {
+                this.carga[index] = carga;
+                index++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public double capacidadeReservada() {
+        return capacidadeReservada();
+    }
+
+    public boolean permitidoReservar(double peso) {
+        double total = 0;
+        for (int i = 0; i < index; i++) {
+            total += carga[i].getPeso();
+        }
+        return (total + peso) <= capacidade;
+    }
+
+    public String getNavio() {
+        return getNavio();
+    }
+}
